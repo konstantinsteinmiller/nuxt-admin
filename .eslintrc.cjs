5d0bd2145703e195dc1eb93da8fe00c467d7c3ba
Konstantin Steinmiller <konstantin.steinmiller@gmail.com>
2024-06-04 15:00:18 +0200

    added admin page with dark mode switch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,7 @@ module.exports = {
     "browser": true,
     "es2021": true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended",
-    'plugin:prettier/recommended'/*,"react-app", "react-app/jest"*/],
+  extends: ["eslint:recommended", 'plugin:prettier/recommended'],
   parserOptions: {
     "ecmaVersion": 12,
     "sourceType": "module",
@@ -15,7 +14,7 @@ module.exports = {
       "jsx": true
     }
   },
-  plugins: ["react", "react-hooks", "import"],
+  plugins: ["import"],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: true,
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+  colorMode: {
+    classSuffix: '',
+  },
 })
